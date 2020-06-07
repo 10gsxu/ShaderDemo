0f8dce8c2d76b0d88516e284cdba2740b12d8835
LeoHui <leoxu@foxmail.com>
2020-06-07 23:09:30 +0800

    fix 像素化效果

---
+++ ShaderDemo/Assets/Script/Pixelate.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Pixelate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Material effectMaterial;
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, effectMaterial);
+    }
+}
